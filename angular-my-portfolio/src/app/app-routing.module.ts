aae9695a8059ea330be2569d6b94927b0b7cd66d
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-10-29 15:17:22 +0700

    adding Navigation and setting Routing Page
    1. Home
    2. Portfolio
    3. Resume
    4. Contact

--- angular-my-portfolio/src/app/app-routing.module.ts
+++ angular-my-portfolio/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'contact', component: ContactComponent },
+  {path: '**', component: HomeComponent, pathMatch:'full'}
 ];
 
 @NgModule({
