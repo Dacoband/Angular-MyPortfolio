4ef328a643f8bbe34e515e85f7a9c3076960bf4a
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-03-06 23:36:48 +0700

    social_media components

--- src/app/components/social-media-links/social-media-links.component.ts
+++ src/app/components/social-media-links/social-media-links.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AppConfig } from '../../../enums/app-data';
 
 @Component({
   selector: 'app-social-media-links',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './social-media-links.component.html',
   styleUrl: './social-media-links.component.scss'
 })
 export class SocialMediaLinksComponent {
-
+  public appConfig = AppConfig;
 }
