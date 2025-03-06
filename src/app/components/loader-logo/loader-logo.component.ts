d9877c3086fe24fbd48f12dcc29650a859c2b728
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-03-06 23:33:40 +0700

    load logo trandorm

---
+++ src/app/components/loader-logo/loader-logo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loader-logo',
+  imports: [],
+  templateUrl: './loader-logo.component.html',
+  styleUrl: './loader-logo.component.scss'
+})
+export class LoaderLogoComponent {
+
+}
